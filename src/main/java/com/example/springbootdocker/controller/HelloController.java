54b0198ddd5351e948fe643cc6f9a778f8cf186c
ttt1804 <trantan1804@gmail.com>
2023-11-11 02:19:27 +0700

    update

--- src/main/java/com/example/springbootdocker/controller/HelloController.java
+++ src/main/java/com/example/springbootdocker/controller/HelloController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 // http://localhost:443/api/hello
 public class HelloController {
 
-    @CrossOrigin(origins = "http://34.143.247.201:443")
+    @CrossOrigin(origins = "http://34.143.247.201")
     @GetMapping("/hello")
     public String hello() {
         return "Hello world";
